00a3733c3e13911e554b7b7ec837331798b6bda6
ofir tsaidi <ofirtsaidi@gmail.com>
2023-12-03 12:36:51 +0200

    feat: initial commit

---
+++ apps/client/src/features/navbar/navbar.tsx
@@ -0,0 +1,28 @@
+import ChatBubbleLeftRightIcon from "@heroicons/react/24/outline/ChatBubbleLeftRightIcon";
+import HomeIcon from "@heroicons/react/24/outline/HomeIcon";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <div className="flex">
+      <div className="flex flex-col items-center justify-evenly w-24 h-full border-r">
+        <Link className="w-1/2" to="home">
+          <HomeIcon />
+        </Link>
+        <Link className="w-1/2" to="chat">
+          <ChatBubbleLeftRightIcon />
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center justify-evenly w-40 h-full border-r">
+        <h1>My Chats</h1>
+        <Link className="w-1/2" to="home">
+          <h2>danielle</h2>
+        </Link>
+        <Link className="w-1/2" to="chat">
+          <h2>kinar</h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
---
+++ apps/client/src/main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Chat } from "./routes/chat";
+import { Root } from "./routes/root";
+import { Home } from "./routes/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/chat",
+        element: <Chat />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
