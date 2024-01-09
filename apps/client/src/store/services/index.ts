e28c760f578065fb2a2bccd214ed3210c60584fa
Ofir Tsaidi <ofirtsaidi@gmail.com>
2024-01-09 10:44:20 +0200

    feat(store): code split server api

--- apps/client/src/store/services/index.ts
+++ apps/client/src/store/services/index.ts
@@ -1,1 +1,2 @@
 export * from './server.api';
+export * from './users.api';
--- apps/client/src/store/services/server.api.ts
+++ apps/client/src/store/services/server.api.ts
@@ -1,17 +1,7 @@
-import { Prisma, Users } from '@prisma/client';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const serverApi = createApi({
   reducerPath: 'serverApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
-  endpoints: (builder) => ({
-    findOrCreateUser: builder.mutation<
-      Users,
-      Pick<Prisma.UsersCreateInput, 'clerkUserId'>
-    >({
-      query: (body) => ({ url: 'users/find-or-create', body, method: 'POST' }),
-    }),
-  }),
+  endpoints: () => ({}),
 });
-
-export const { useFindOrCreateUserMutation } = serverApi;
