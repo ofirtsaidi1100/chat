494d05c1b0a4b89e762b0f8417b4331ada3b9dbc
Ofir Tsaidi <ofirtsaidi@gmail.com>
2024-03-21 10:30:09 +0200

    refactor: delete app provider and controller

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   exports: [],
 })
 export class AppModule {}
