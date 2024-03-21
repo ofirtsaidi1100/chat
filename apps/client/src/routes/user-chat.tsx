f6dee03b9f92e202a4d0ac5a3d810ffa3bf59564
Ofir Tsaidi <ofirtsaidi@gmail.com>
2024-03-21 10:30:25 +0200

    feat: design textarea

--- apps/client/src/routes/user-chat.tsx
+++ apps/client/src/routes/user-chat.tsx
@@ -1,21 +1,34 @@
 import { useParams } from 'react-router-dom';
 import { useFindUserByIdQuery } from '../store';
 import { Send } from 'lucide-react';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 export const UserChat: React.FC = () => {
   const { userId } = useParams();
   const { data: user } = useFindUserByIdQuery(userId ?? '');
-  const [textareaValue, setTextareaValue] = useState('');
-  const [textareaRows, setTextareaRows] = useState(1);
+  const textareaRef = useRef<HTMLTextAreaElement>(null); // Reference for textarea element
 
+  // Function to adjust textarea height based on content
+  const adjustTextareaHeight = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto'; // Reset textarea height
+      const maxRows = 4;
+      const lineHeight = parseFloat(
+        getComputedStyle(textareaRef.current).lineHeight || '0'
+      );
+      const maxHeight = maxRows * lineHeight;
+      textareaRef.current.style.height = `${Math.min(
+        textareaRef.current.scrollHeight,
+        maxHeight
+      )}px`; // Set textarea height to content height or maximum height
+    }
+  };
+
+  // Event handler for textarea change
   const handleTextareaChange = (
     event: React.ChangeEvent<HTMLTextAreaElement>
   ) => {
-    const { value } = event.target;
-    setTextareaValue(value);
-    const numLines = value.split('\n').length;
-    setTextareaRows(Math.min(4, numLines)); // Limit to maximum of 4 rows
+    adjustTextareaHeight(); // Adjust textarea height on change
   };
 
   return (
@@ -35,10 +48,9 @@ export const UserChat: React.FC = () => {
       <div className="w-full h-full flex flex-col justify-end items-center p-4">
         <div className="p-4 w-full h-fit flex gap-4">
           <textarea
+            ref={textareaRef} // Assign the ref to the textarea element
             className="border py-2 px-4 rounded-3xl w-full resize-none overflow-auto"
-            value={textareaValue}
-            rows={textareaRows}
-            onChange={handleTextareaChange}
+            onChange={handleTextareaChange} // Call handleTextareaChange on change
           />
           <button className="rounded-full bg-blue-400 w-fit h-fit p-3 flex justify-center items-center mt-auto [&_*]:stroke-white">
             <Send className="h-6 w-6" />
